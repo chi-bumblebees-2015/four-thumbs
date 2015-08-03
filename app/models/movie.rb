7188aba95e88b97ae724437e58af708e13365898
tra38 <tra38@nau.edu>
2015-08-03 17:36:41 -0500

    Move 'review' business logic from view to model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,17 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
+
+  def trusted_reviews
+  	reviews.joins(:user).where('users.trusted' => true).order(rating: :desc)
+  end
+
+  def user_reviews
+  	reviews.joins(:user).where('users.trusted' => false).order(rating: :desc)
+  end
+
+  def all_reviews
+  	reviews.order(rating: :desc)
+  end
+
+
 end
