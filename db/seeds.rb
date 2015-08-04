4a4f111b0cd618a79c741c936dd965c64414a3e4
Tariq Ali <tra38@nau.edu>
2015-08-04 13:35:26 -0500

    Fix bugs in Seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,9 @@
-30.times do
-  Movie.create(name: "#{Faker::Book.title} (#{rand(1970..2015)} film)", aggregate_rating: rand(3..100), plot_summary: generate_plot)
-end
-
-20.times do |number|
-  User.create(name: "user#{number}", password: "superdogs", email: "test#{number}@gmail.com", trusted: [true, false].sample )
-end
-
-40.times do
-  Review.create(user: random_user, movie: random_movie, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "#{Faker::Book.title}", rating: rand(0..4) )
-end
-
-50.times do
-  Comment.create(user: random_user, review: random_review, content: "#{Faker::Lorem.sentence(1, true)}")
-end
-
 #Helper Methods
 def generate_plot
   heroes = ["A squadron of robots", "#{Faker::Name.name}, a #{Faker::Name.title} down-on-their-luck","The city of #{Faker::Address.city}","A loving couple","A tiny megacorp named #{Faker::Company.name}"].sample
-  action = "must #{Faker::Hacker.verb} #{Faker::Hacker.adjective} #{Faker::Hacker.noun} before the evil #{Faker::Hacker.abbreviation} begin Project #{Faker::App.name}."
-  heroes+" "+action
+  must_do_this =["#{Faker::Hacker.verb} #{Faker::Hacker.adjective} #{Faker::Hacker.noun}", "#{Faker::Company.bs}", "determine the meaning of #{Faker::Company.catch_phrase}"].sample
+  before_the_deadline = "before the evil #{Faker::Hacker.abbreviation} begin Project #{Faker::App.name}."
+  heroes+" "+must_do_this+" "+before_the_deadline
 end
 
 def random_movie
@@ -31,4 +16,21 @@ end
 
 def random_review
   Review.all.sample
+end
+
+#Actual Seeding
+30.times do
+  Movie.create(name: "#{Faker::App.name} (#{rand(1970..2015)} film)", aggregate_rating: rand(3..100), plot_summary: generate_plot)
+end
+
+20.times do |number|
+  User.create(name: "user#{number}", password: "superdogs", email: "test#{number}@gmail.com", trusted: [true, false].sample )
+end
+
+40.times do
+  Review.create(user: random_user, movie: random_movie, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "#{Faker::App.name}", rating: rand(0..4) )
+end
+
+50.times do
+  Comment.create(user: random_user, review: random_review, content: "#{Faker::Lorem.sentence(1, true)}")
 end
