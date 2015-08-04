f7775787c1b67920ac5f24170c7d397e398859e9
Michael Ramuta <michaelramuta@gmail.com>
2015-08-04 14:22:48 -0500

    made final changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,10 +18,12 @@ class Movie < ActiveRecord::Base
     user_total = self.reviews.select{|review| !review.user.trusted }.map{|review| review.rating}.reduce(:+)
     trusted_count = self.reviews.select{|review| review.user.trusted }.count
     trusted_total = self.reviews.select{|review| review.user.trusted }.map{|review| review.rating}.reduce(:+)
+    review_number_total = self.reviews.map{|review| review.user.reviews.count * review.rating}.reduce(:+)
+    review_number_count = self.reviews.map{|review| review.user.reviews.count}.reduce(:+)
 
     trusted_multiplier = 2
-    total = trusted_total*trusted_multiplier + user_total ||= 0
-    count = trusted_count*trusted_multiplier + user_count ||= 1
+    total = trusted_total*trusted_multiplier + user_total + review_number_total ||= 0
+    count = trusted_count*trusted_multiplier + user_count + review_number_count ||= 1
     return (total.to_f/count.to_f).round
   end
 
