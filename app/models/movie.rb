acca0283205c1852f64f338ae91207e76afcc376
Michael Ramuta <michaelramuta@gmail.com>
2015-08-04 11:13:59 -0500

    working on movie score

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,15 @@ class Movie < ActiveRecord::Base
   	reviews.joins(:user).order(rating: :desc)
   end
 
+  def movie_score
+    count = self.reviews.count
+    total = self.reviews.map{|review| review.rating}.reduce(:+)
+    if count == 0
+      return 0
+    else
+      return (total.to_f/count.to_f).round
+    end
+  end
 
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,6 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   acts_as_votable
 
+  validates :rating, :inclusion=> { :in => [0,1,2,3,4]}
 
 end
