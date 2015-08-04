ba7bc33b0d5c5e23db3de858614e826fe2f718bb
Tariq Ali <tra38@nau.edu>
2015-08-04 09:08:30 -0500

    Add tests for movies

--- spec/features/movie_spec.rb
+++ spec/features/movie_spec.rb
@@ -6,4 +6,20 @@ feature "User viewing a movie" do
     find('#movie-1').click
     expect(page).to have_content(Movie.find(1).name)
   end
+
+  scenario "user can see 'Trusted Reviews'" do
+    visit '/movies/1'
+    expect(page).to have_content("Trusted Reviews")
+  end
+
+  scenario "user can see 'User Reviews'" do
+    visit '/movies/1'
+    expect(page).to have_content("User Reviews")
+  end
+
+  scenario "user can see 'all reviews'" do
+    visit '/movies/1'
+    expect(page).to have_content ("All Reviews")
+  end
+
 end
