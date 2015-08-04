4dcd4ac2c86e5e89b8a25b800cb43ce2bf3c0183
Tariq Ali <tra38@nau.edu>
2015-08-04 13:28:33 -0500

    Overhaul seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,29 @@
-movie1 = Movie.create(name: "Movie 1", aggregate_rating: 100, plot_summary: "stuff happens")
-movie2 = Movie.create(name: "Blade Runner", aggregate_rating: 99, plot_summary: "never seen it, but i hear harrison ford is a robot")
-movie3 = Movie.create(name: "Two Guns", aggregate_rating: 80, plot_summary: "Denzel does stuff")
-movie4 = Movie.create(name: "Three Guns", aggregate_rating: 3, plot_summary: "Eh")
+30.times do
+  Movie.create(name: "#{Faker::Book.title} (#{rand(1970..2015)} film)", aggregate_rating: rand(3..100), plot_summary: generate_plot)
+end
 
+20.times do |number|
+  User.create(name: "user#{number}", password: "superdogs", email: "test#{number}@gmail.com", trusted: [true, false].sample )
+end
 
-user1 = User.create(name: "user1", password: "superdogs", email: "test@gmail.com", trusted: true)
-user2 = User.create(name: "user2", password: "superdogs", email: "test2@gmail.com", trusted: false)
-user3 = User.create(name: "user3", password: "superdogs", email: "test3@gmail.com", trusted: true)
+40.times do
+  Review.create(user: random_user, movie: random_movie, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "#{Faker::Book.title}", rating: rand(0..4) )
+end
 
+50.times do
+  Comment.create(user: random_user, review: random_review, content: "#{Faker::Lorem.sentence(1, true)}")
+end
 
-Review.create(user: user1, movie: movie1, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 4)
-Review.create(user: user1, movie: movie2, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 3)
-Review.create(user: user1, movie: movie3, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 2)
-Review.create(user: user2, movie: movie1, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 4)
-Review.create(user: user3, movie: movie2, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 4)
-Review.create(user: user3, movie: movie4, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 4)
-Review.create(user: user3, movie: movie3, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "title of review", rating: 4)
+#Helper Methods
+def generate_plot
+  heroes = ["A squadron of robots", "#{Faker::Name.name}, a #{Faker::Name.title} down-on-their-luck","The city of #{Faker::Address.city}","A loving couple","A tiny megacorp named #{Faker::Company.name}"].sample
+  action = "must #{Faker::Hacker.verb} #{Faker::Hacker.adjective} #{Faker::Hacker.noun} before the evil #{Faker::Hacker.abbreviation} begin Project #{Faker::App.name}."
+  heroes+" "+action
+end
+
+def random_movie
+  Movie.all.sample
+end
 
 def random_user
   User.all.sample
@@ -23,8 +31,4 @@ end
 
 def random_review
   Review.all.sample
-end
-
-10.times do
-  Comment.create(user: random_user, review: random_review, content: "#{Faker::Lorem.sentence(1, true)}")
 end
