1d5a5381a1f37818aab84939eb99f29b6cbffa88
Tariq Ali <tra38@nau.edu>
2015-08-04 15:03:32 -0500

    Fix user login test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User can login' do
   scenario "user can click to visit login page" do
     visit '/'
-    find('#login').click
+    find('#login:first-child').click
     expect(page).to have_content("Username:")
     expect(page).to have_content("Password:")
   end
@@ -11,8 +11,8 @@ feature 'User can login' do
   scenario "user can log in" do
     visit '/login'
     within("form") do
-      fill_in 'Username',:with => "user1"
-      fill_in 'Password', :with => "superdogs"
+      fill_in 'name',:with => "user1"
+      fill_in 'password', :with => "superdogs"
     end
     click_button 'Submit'
     expect(page).to have_content("Welcome, user1")
