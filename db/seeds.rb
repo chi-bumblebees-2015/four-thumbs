3a581911f69e493c3d9642160e63e8b2dc23349a
Tariq Ali <tra38@nau.edu>
2015-08-04 13:40:46 -0500

    Increase number of reviews, comments, and users that the seed data will generate

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,14 +23,14 @@ end
   Movie.create(name: "#{Faker::App.name} (#{rand(1970..2015)} film)", aggregate_rating: rand(3..100), plot_summary: generate_plot)
 end
 
-20.times do |number|
+200.times do |number|
   User.create(name: "user#{number}", password: "superdogs", email: "test#{number}@gmail.com", trusted: [true, false].sample )
 end
 
-40.times do
+400.times do
   Review.create(user: random_user, movie: random_movie, content: "#{Faker::Lorem.paragraph(2, true, 4)}", title: "#{Faker::App.name}", rating: rand(0..4) )
 end
 
-50.times do
+500.times do
   Comment.create(user: random_user, review: random_review, content: "#{Faker::Lorem.sentence(1, true)}")
 end
