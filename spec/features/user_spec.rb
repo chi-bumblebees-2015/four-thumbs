4570364a92a4f326d987cc6e871432b7e1b4dc55
Tariq Ali <tra38@nau.edu>
2015-08-04 13:15:25 -0500

    Attempt to fix tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,4 +7,15 @@ feature 'User can login' do
     expect(page).to have_content("Username:")
     expect(page).to have_content("Password:")
   end
+
+  scenario "user can log in" do
+    visit '/login'
+    within("form") do
+      fill_in 'Username',:with => "user1"
+      fill_in 'Password', :with => "superdogs"
+    end
+    click_button 'Submit'
+    expect(page).to have_content("Welcome, user1")
+  end
+
 end
