35e4b66f988a5284380572d80791ad7e0d88ec36
Michael Ramuta <michaelramuta@gmail.com>
2015-08-05 11:20:16 -0500

    changed popular reviews to new reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -21,4 +21,8 @@ class Review < ActiveRecord::Base
     self.all.sort{|a,b| b.get_upvotes.size <=> a.get_upvotes.size}[0..9]
   end
 
+  def self.recent_reviews
+    self.all.sort{|a,b| b.created_at <=> a.created_at}[0..9]
+  end
+
 end
