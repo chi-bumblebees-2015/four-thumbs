570fb7dbf8c9c06ae7a17dfa8f0fdb42a49e6274
Tariq Ali <tra38@nau.edu>
2015-08-05 10:21:28 -0500

    Expand writing reviews integration tests

--- spec/features/movie_spec.rb
+++ spec/features/movie_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 feature "User viewing a movie" do
   scenario "when user clicks on a movie from the homepage" do
-    visit '/'
-    find('#movie-1').click
+    visit '/movies/1'
     expect(page).to have_content(Movie.find(1).name)
   end
 
